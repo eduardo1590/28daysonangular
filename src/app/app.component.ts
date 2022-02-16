b930ef8b15760a901f32d5d8a5c6b27f48d6e624
eduardo1590 <eduardo1590@gmail.com>
2022-02-16 12:14:23 -0400

    dia 9 (event binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit{
   title = '28-days-on-angular';
+
+  cities = ['Caracas', 'Bogota', 'Madrid'];
+  name!: string;
+  selection!: string;
+
+  ngOnChanges(){
+    console.log('On Changes');
+  }
+
+  ngOnInit(){
+    console.log('On Init');
+  }
+  onCityClicked(city: string): void{
+    console.log('city->' + city);
+    this.selection = city;
+  }
+
+  onClear(){
+    this.selection = "";
+  }
 }
